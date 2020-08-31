a2c17af5262f46a10304b2fa0a36c462656c2606
singlapayal43@gmail.com <singlapayal43@gmail.com>
2020-08-31 06:04:01 +0530

    commited

---
+++ dictionaries/add_delete_dict.py
@@ -0,0 +1,10 @@
+data1 = dict(name= "payal", age=19)
+#how to insert data in a dict
+data1["hometown"]="nabha"
+print(data1)
+
+
+#pop method
+print(data1.pop('hometown'))
+#print(data1.pop('nabha'))
+#popitem() pop the random key
--- sets/set_maths.py
+++ sets/set_maths.py
@@ -4,6 +4,8 @@ s1={1,54,6,7,5,2,3}
 union=s|s1 #known as pipe |
 print(union)
 #intersection &
+
+
 s={1,2,4,5,2,3,4}
 s1={1,54,6,7,5,2,3}
 intersection=s&s1
