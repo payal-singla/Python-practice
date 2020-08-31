2043e9b17cdb42468daa2e71e19e1eec6ed1cfa7
singlapayal43@gmail.com <singlapayal43@gmail.com>
2020-08-31 06:04:01 +0530

    commited

--- oops/objetcs.py
+++ oops/objetcs.py
@@ -8,7 +8,7 @@ class Person:
     def above_18(self):
         return self.age>18
 p1=Person("payal",'singla',19)
-p2=Person("keshav",'singla',14)
+p2=Person("keshav",'singla',15)
 print(p1.first)
 print(p1.full())
 print(p1.above_18())
